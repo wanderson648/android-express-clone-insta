9032b3991058f891936c43b14ccc21d8183d8753
wanderson648 <testepedroca@gmail.com>
2023-01-20 14:32:44 -0300

    Desenhando Tela de Login

---
+++ app/src/main/java/com/wo/cloneinsta/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.wo.cloneinsta.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wo.cloneinsta.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
