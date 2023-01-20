570154716999c7b15a7154dd25b0862e37890239
wanderson648 <testepedroca@gmail.com>
2023-01-20 15:45:21 -0300

    Validação de formulário

--- app/src/main/java/com/wo/cloneinsta/view/LoginActivity.kt
+++ app/src/main/java/com/wo/cloneinsta/view/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.wo.cloneinsta.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputLayout
 import com.wo.cloneinsta.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<TextInputLayout>(R.id.login_edit_email_input)
+            .error = "Esse email é inválido"
     }
 }
