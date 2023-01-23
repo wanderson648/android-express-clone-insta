6c3008cb3e7ea50a33a9ead13ed1bbc031d0ca2a
wanderson648 <testepedroca@gmail.com>
2023-01-23 19:03:03 -0300

    Dialog customizada

--- app/src/main/java/com/wo/cloneinsta/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/com/wo/cloneinsta/register/view/RegisterPhotoFragment.kt
@@ -1,11 +1,13 @@
 package com.wo.cloneinsta.register.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.wo.cloneinsta.R
+import com.wo.cloneinsta.common.view.CustomDialog
 
 class RegisterPhotoFragment: Fragment() {
 
@@ -15,6 +17,28 @@ class RegisterPhotoFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+
+//        customDialog.setTitle(R.string.app_name)
+        customDialog.addButton(R.string.photo, R.string.gallery) {
+            when(it.id) {
+                R.string.photo -> {
+                    Log.i("Test", "Photo clicked")
+                }
+                R.string.gallery -> {
+                    Log.i("Test", "Gallery clicked")
+                }
+            }
+        }
+
+        customDialog.show()
+
+
     }
 
 }
